9857da56acf32a8c25b88262cb806e583c165ae0
Сергей Постнов <toosergey@yandex.ru>
2023-03-26 12:12:12 +0300

    Commit_11

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,8 +2,8 @@ import os
 from datetime import timedelta
 
 from django.core.management.utils import get_random_secret_key
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 
 SECRET_KEY = get_random_secret_key()
 
